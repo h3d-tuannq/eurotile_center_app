cac7c51310a4a382b926ed85c0e9e102b9e5d5f2
tuann <tuannq@projectkit.net>
2020-10-06 07:42:46 +0700

    Giao diện Product.

---
+++ src/views/product/CollectionDetailScreen.js
@@ -0,0 +1,256 @@
+import React from 'react'
+import {Text, View, Button, StyleSheet, Dimensions, ScrollView, TouchableOpacity, Image} from 'react-native'
+import ScrollableTabView, { ScrollableTabBar,DefaultTabBar  }  from 'react-native-scrollable-tab-view';
+import CollectionTab from './CollectionTab'
+import MyCustomizeTabBar from  '../../com/common/tabar/MyCustomizeTabBar'
+import NetCollection from '../../net/NetCollection'
+import Def from '../../def/Def'
+const {width, height} = Dimensions.get('window');
+
+import Carousel from 'react-native-snap-carousel';
+import Pagination from "react-native-snap-carousel/src/pagination/Pagination";
+import Style from '../../def/Style';
+
+const PROGRAM_IMAGE_WIDTH = (width - 30-8) /2;
+const PROGRAM_IMAGE_HEIGHT = (width - 30-8) /2;
+const carouselItems = [
+    {
+        id:1,
+        image_path : 'https://eurotiledev.house3d.net/data/eurotileData/collection/202009/24/1/main_img.jpg',
+    },
+    {
+        id:2,
+        image_path : 'https://eurotiledev.house3d.net/data/eurotileData/collection/202009/30/2/main_img.jpg',
+    }
+];
+
+class CollectionDetailScreen extends React.Component {
+
+
+    state = {
+        // collection_data: null,
+        stateCount: 0.0,
+        configMenu: Def.config_collection_menu,
+        slide_data : this.props.route.params.item ? this.getImageForCollection(this.props.route.params.item) : carouselItems,
+        item: this.props.route.params.item,
+    };
+
+    constructor(props){
+        super(props);
+        this.onGetCollectionSuccess     = this.onGetCollectionSuccess.bind(this);
+        this.onGetCollectionFalse     = this.onGetCollectionFalse.bind(this);
+        this.formatText    = this.formatText.bind(this);
+        this.refresh     = this.refresh.bind(this);
+
+        let item = this.props.route.params.item;
+        let collectionImages = [this.props.route.params.item.image_path];
+        if(item.sub_images){
+            let subImgs = item.sub_images.split(',');
+            subImgs = subImgs.map(x => Def.URL_CONTENT_BASE + x);
+            collectionImages = collectionImages.concat(subImgs);
+        }
+
+
+
+        Def.mainNavigate = this.props.navigation;
+
+        // if(!Def.collection_data) {
+        //     NetCollection.listCollection(this.onGetCollectionSuccess, this.onGetCollectionFalse);
+        // }
+        // else if (!Def.config_collection_menu) {
+        //     Def.config_collection_menu = this.createConfigData(Def.collection_data);
+        //     this.setState({configMenu: Def.config_news_menu});
+        // }
+
+    }
+
+    getImageForCollection(item){
+        let collectionImages = [{image_path:item.image_path}];
+        if(item.sub_images){
+            let subImgs = item.sub_images.split(',');
+            subImgs = subImgs.map(x => {
+                return {image_path:Def.URL_CONTENT_BASE + x}
+            });
+            collectionImages = collectionImages.concat(subImgs);
+        }
+        console.log("Collection-Slide", JSON.stringify(collectionImages));
+        return collectionImages;
+    }
+
+    refresh()
+    {
+        //NetChannel.listChannel(this.onChannelSuccess,this.onChannelFailed);
+        this.setState({ stateCount: Math.random() });
+    }
+
+    onGetCollectionSuccess(data){
+        // console.log(Object.entries(data["data"]));
+        Object.entries(data["data"]).map((prop, key) => {
+            // console.log('Start');
+            // console.log(prop[0]);
+            // console.log(prop[1]["data"]);
+            // console.log('Start');
+        });
+        this.setState({ collection_data: data["data"] });
+        Def.collection_data = data["data"];
+        Def.config_collection_menu = this.createConfigData(data["data"]) ;
+        this.setState({ configMenu: Def.config_collection_menu});
+    }
+
+    createConfigData(data){
+
+
+        if(data){
+            let configData =  Object.entries(data).map((prop, key) => {
+                // console.log("Props : " + JSON.stringify(prop));
+                return {key: prop[0],name_vi:prop[1]["name_vi"], hidden:0, data:prop[1]["data"]};
+            });
+            Object.entries(configData).map((prop, key) => {
+                console.log("start" + key);
+                console.log("prop[0]" + prop[0]);
+                console.log("prop[1]" + prop[1]["name_vi"]);
+
+                console.log("data" + prop[1]["data"]);
+
+                console.log("end");
+            });
+            return configData;
+        }
+
+    }
+
+    onGetCollectionFalse(data){
+        console.log("false data : " + data);
+    }
+
+    formatText(text){
+        let rs = text;
+        if(text && text.length > 10){
+            rs = text.substring(0, 20) ;
+        }
+        return rs;
+    }
+
+    shouldComponentUpdate(){
+        // this.setState({ configMenu: Def.config_news_menu});
+        // console.log('SortData ddd:' + JSON.stringify(this.props.route));
+        return true;
+    }
+
+    getNewDataByConfigKey(key){
+
+    }
+
+    get pagination () {
+        const { slide_data, activeSlide } = this.state;
+        return (
+            <Pagination
+                dotsLength={slide_data.length}
+                activeDotIndex={activeSlide}
+                containerStyle={{ position:'absolute',top : 5, right : 0, width : slide_data.length  * 5,  paddingVertical: 5  }}
+                dotContainerStyle={{marginHorizontal : 6,}}
+                dotStyle={{
+                    width: 10,
+                    height: 10,
+                    borderRadius: 5,
+                    borderWidth : 1,
+                    backgroundColor : 'rgba(179, 179, 179, 0.92)',
+                }}
+                inactiveDotStyle={{
+
+                    backgroundColor: 'rgba(255, 255, 255, 0.92)'
+                }}
+                inactiveDotOpacity={1}
+                inactiveDotScale={1}
+            />
+        );
+    }
+    renderItem = ({item, index}) => {
+
+        return (
+            <View key={index} style={Style.styles.cardStyle}>
+                <TouchableOpacity >
+                    <Image  style = {[Style.styles.cardImg, {resizeMode : 'stretch'}]} source={{ uri: item.image_path}} />
+                </TouchableOpacity>
+            </View>
+        );
+
+    }
+
+
+    render() {
+        const {navigation} = this.props;
+        // const configMenu = Def.config_collection_menu;
+        return (
+            <View style={{flex:1}}>
+                <View style={Style.styles.carousel}>
+                    <Carousel
+                        ref={(c) => { this._carousel = c; }}
+                        // keyExtractor={(item, index) => `${item.id}--${item.index}`}
+                        data={this.state.slide_data}
+                        renderItem={this.renderItem}
+                        itemWidth={width}
+                        sliderWidth={width}
+                        inactiveSlideOpacity={1}
+                        inactiveSlideScale={1}
+                        activeSlideAlignment={'start'}
+                        loop={true}
+                        autoplay={true}
+                        autoplayInterval={5000}
+                        onSnapToItem={(index) => this.setState({ activeSlide: index }) }
+                    />
+                    { this.pagination }
+                </View>
+
+                {/*<ScrollableTabView  renderTabBar={() => <MyCustomizeTabBar navigation={navigation} />}  >*/}
+                    {/*{*/}
+                        {/*configMenu && Object.entries(configMenu).map((prop, key) => {*/}
+                            {/*if((prop[1]["hidden"]) == 0){*/}
+                                {/*return (*/}
+                                    {/*<CollectionTab key ={prop[0] + "acv"}  navigation={navigation} refresh={this.refresh} tabLabel={this.formatText(prop[1]["name_vi"])} title={this.formatText(prop[1]["name_vi"])} data={prop[1]["data"]}  />*/}
+                                {/*);*/}
+                            {/*}*/}
+                        {/*})*/}
+                    {/*}*/}
+                {/*</ScrollableTabView>*/}
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex : 1,
+        paddingLeft: 15,
+        // justifyContent: 'flex-start',
+        // marginVertical : 5,
+        marginBottom : 125,
+        backgroundColor: '#fff'
+    },
+    slider: {
+        justifyContent: 'center',
+        paddingTop: 5,
+        padding: 8,
+        height: 120,
+        borderRadius: 5,
+        backgroundColor: "#e6e6e6",
+        marginRight : 15
+    },
+    cardStyle: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: width-20,
+        height: width/2,
+
+    },
+    programListStyle : {
+
+    },
+    itemImage: {
+        width: PROGRAM_IMAGE_WIDTH -5,
+        height : PROGRAM_IMAGE_HEIGHT -5,
+        borderRadius: 5,
+    },
+});
+
+export default CollectionDetailScreen
---
+++ src/views/product/CollectionTab.js
@@ -0,0 +1,88 @@
+import React from 'react'
+import {Text, View, Button, StyleSheet, Dimensions, ScrollView, FlatList, Platform} from 'react-native'
+import CollectionItemrenderer from '../../com/item-render/CollectionItemrenderer'
+import Style from "../../../src/def/Style";
+
+const {width, height} = Dimensions.get('window');
+
+
+const PROGRAM_IMAGE_WIDTH = (width - 30) /2;
+const PROGRAM_IMAGE_HEIGHT = (width - 30) /2;
+
+
+class CollectionTab extends React.Component {
+    constructor(props){
+        super(props);
+        this.itemClick = this.itemClick.bind(this);
+    }
+
+    itemClick(item){
+        console.log(item.id);
+        this.props.navigation.navigate('collection-detail-screen', { item:item});
+
+    }
+
+    render() {
+        const {navigation} = this.props;
+        const renderItem = ({ item }) => (
+            <CollectionItemrenderer click={this.itemClick}  item={item} favorite={true} styleImage={{width:PROGRAM_IMAGE_WIDTH -2, height:PROGRAM_IMAGE_HEIGHT-5, marginRight:6, marginBottom : 5 }} />
+        );
+        return (
+            <View style={styles.container}>
+                <View>
+                    <Text style={[Style.text_styles.titleText, {textAlign:'center', paddingVertical:5}]}>
+                        Bộ sưu tập
+                    </Text>
+                </View>
+                <View style={{marginTop :8, flex:1}}>
+                    <FlatList
+                        data={this.props.data}
+                        renderItem={renderItem}
+                        keyExtractor={item => item.id}
+                        showsHorizontalScrollIndicator={false}
+                        numColumns={2}
+                    />
+                </View>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex : 1,
+        paddingLeft: 15,
+        // justifyContent: 'flex-start',
+        // marginVertical : 5,
+        // marginBottom : 5,
+        backgroundColor: '#fff',
+        paddingTop : 5
+    },
+    slider: {
+        justifyContent: 'center',
+        paddingTop: 5,
+        padding: 8,
+        height: 120,
+        borderRadius: 5,
+        backgroundColor: "#e6e6e6",
+        marginRight : 15
+    },
+    cardStyle: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: width-20,
+        height: width/2,
+
+    },
+    programListStyle : {
+
+    },
+    itemImage: {
+        width: PROGRAM_IMAGE_WIDTH -5,
+        height : PROGRAM_IMAGE_HEIGHT -5,
+        borderRadius: 5,
+    },
+
+});
+
+export default CollectionTab;
