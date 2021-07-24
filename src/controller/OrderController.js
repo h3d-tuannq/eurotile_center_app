880c6f3e2efece58385ab51a97767662410449d9
tuannq <tuannq@house3d.net>
2021-07-24 17:32:38 +0700

    Xóa thử viện thừa cập nhật thư viện mới

--- src/controller/OrderController.js
+++ src/controller/OrderController.js
@@ -51,14 +51,14 @@ export default class OrderController{
     static getOrder(callback,errCallback ) {
         if(Def.user_info){
             console.log("Call Get Order");
-            Net.sendRequest(callback ? callback : this.getOrderSuccess,errCallback? errCallback: this.getOrderFalse ,Def.URL_BASE + "/api/order/get-order" ,Def.POST_METHOD, {partner_id:Def.user_info['id']});
+            Net.sendRequest(callback ? callback : this.getOrderSuccess,errCallback? errCallback: this.getOrderFalse ,Def.URL_BASE + "/api/order/get-order" ,Def.POST_METHOD, {partnerId:Def.user_info['id']});
         } else {
             AsyncStorage.getItem('user_info').then((value) => {
                 if(value){
                     Def.user_info = JSON.parse(value);
                     Def.username = Def.user_info['user_name'];
                     Def.email = Def.user_info['email'];
-                    Net.sendRequest(callback ? callback : this.getOrderSuccess,errCallback? errCallback: this.getOrderFalse ,Def.URL_BASE + "/api/order/get-order" ,Def.POST_METHOD, {partner_id:Def.user_info['id']});
+                    Net.sendRequest(callback ? callback : this.getOrderSuccess,errCallback? errCallback: this.getOrderFalse ,Def.URL_BASE + "/api/order/get-order" ,Def.POST_METHOD, {partnerId:Def.user_info['id']});
                 }
             });
         }
